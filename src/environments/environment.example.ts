800f2aa3645e285fbd256a07789e6420d4bdc76a
Dennis Luke Owuor <dennisluke11@gmail.com>
2026-01-28 11:10:45 +0200

    chore: ignore real env files and add example environments

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,18 +1,20 @@
 export const environment = {
   production: false,
   openaiApiKey: '',
-  geminiApiKey: '', // Set via .env file or environment variable (see README.md)
-  useFirebaseFunctions: false, // Set to true to use Firebase Functions (recommended for production)
+  geminiApiKey: '',
+  useFirebaseFunctions: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBh_luw17BOgqmndHRmLpZrDYnV0p0bV0I',
-    authDomain: 'learner-s-career-path.firebaseapp.com',
+    apiKey: 'YOUR_FIREBASE_API_KEY_HERE',
+    authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN_HERE',
     projectId: 'learner-s-career-path',
     storageBucket: 'learner-s-career-path.firebasestorage.app',
     messagingSenderId: '2366881853',
     appId: '1:2366881853:web:0e5966a7fac784b20c6a54',
-    measurementId: 'G-CY6RMKXW1V'
+    measurementId: 'G-XXXXXXXXXX'
   },
   // Firebase App Check - reCAPTCHA v3 site key
   // Get this from Firebase Console > Build > App Check > Register app
-  recaptchaSiteKey: '' // Add your reCAPTCHA v3 site key here
+  recaptchaSiteKey: ''
 };
+
+
---
+++ src/environments/environment.prod.example.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  openaiApiKey: '',
+  geminiApiKey: '',
+  useFirebaseFunctions: true,
+  firebaseConfig: {
+    apiKey: 'YOUR_FIREBASE_API_KEY_HERE',
+    authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN_HERE',
+    projectId: 'learner-s-career-path',
+    storageBucket: 'learner-s-career-path.firebasestorage.app',
+    messagingSenderId: '2366881853',
+    appId: '1:2366881853:web:0e5966a7fac784b20c6a54',
+    measurementId: 'G-XXXXXXXXXX'
+  },
+  // Firebase App Check - reCAPTCHA v3 site key
+  // Get this from Firebase Console > Build > App Check > Register app
+  recaptchaSiteKey: ''
+};
+
+
